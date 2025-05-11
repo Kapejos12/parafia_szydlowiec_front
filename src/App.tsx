05a6afc403ae1f65b60641686e396670445f8e93
Kacper Makowski <kacper.makowski1@gmail.com>
2025-05-12 01:04:22 +0200

    Create view for parish office (#12)
    * Change colors pallete. Create some changes for actuals components
    
    * Fix some problems with views and their styles. Create Parish office page and styles

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,10 @@ import LayoutComponent from './components/Layout.component/Layout.component'
 
 import './App.css'
 import HomePage from './pages/Home/Home.page'
-import NewsPage from './pages/News.page'
 import PageNotFound from './pages/404.page'
 import NewsDetailComponent from './components/NewsDetail.component/NewsDetail.component'
 import ContactPage from './pages/Contact/Contact.page'
+import ParishOffice from './pages/Office/ParishOffice.page'
 
 
 function App() {
@@ -16,8 +16,8 @@ function App() {
       <Route path="/" element={<LayoutComponent />} >
         <Route index element={<HomePage />} />
         <Route path="aktualnosci/:slug" element={<NewsDetailComponent />} />
-        <Route path="ogloszenia" element={<NewsPage />} />
         <Route path="kontakt" element={<ContactPage />} />
+        <Route path="kancelaria" element={<ParishOffice />} />
         <Route path="*" element={<PageNotFound />} />
       </Route>
     </Routes>
--- src/pages/Contact/Contact.page.tsx
+++ src/pages/Contact/Contact.page.tsx
@@ -1,43 +1,54 @@
 import React from 'react';
-
 import './ContactPage.css';
 
-
 const ContactPage: React.FC = () => {
-
-
     return (
         <div className="contact-page">
-            <div className="map-container">
+            {/* Sekcja z mapą na górze */}
+            <div className="map-section">
                 <h2>Znajdź nas</h2>
-                <div className="map-placeholder">
-                    {/* Tutaj można zintegrować Google Maps lub inną mapę */}
-                    <div className="map-overlay">
-                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4293.162317616383!2d20.858539062322965!3d51.22365858111671!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4718489bc14d641f%3A0x476872aad38e020!2zS2_Fm2Npw7PFgiDFm3cuIFp5Z211bnRhIHcgU3p5ZMWCb3djdQ!5e0!3m2!1spl!2spl!4v1746563359486!5m2!1spl!2spl" width="100%" height="100%" style={{ border: 0 }} allowFullScreen loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
-                    </div>
+                <div className="map-container">
+                    <iframe
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4293.162317616383!2d20.858539062322965!3d51.22365858111671!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4718489bc14d641f%3A0x476872aad38e020!2zS2_Fm2Npw7PFgiDFm3cuIFp5Z211bnRhIHcgU3p5ZMWCb3djdQ!5e0!3m2!1spl!2spl!4v1746563359486!5m2!1spl!2spl"
+                        allowFullScreen
+                        loading="lazy"
+                        referrerPolicy="no-referrer-when-downgrade"
+                    />
                 </div>
             </div>
 
+            {/* Sekcja z informacjami kontaktowymi w jednym wierszu */}
             <div className="container">
-                <div className="contact-content">
-                    <div className="info-icon">
-                        <i className="icon-location"></i>
-                    </div>
-                    <h3>Adres</h3>
-                    <p>ul. Zakościelna 13</p>
-                    <p>26-500 Szydłowiec</p>
+                <div className="contact-info-section">
+                    <div className="contact-info-row">
+                        {/* Karta z adresem */}
+                        <div className="info-card">
+                            <div className="info-icon">
+                                <i className="pi pi-map-marker"></i>
+                            </div>
+                            <h3>Adres</h3>
+                            <p>ul. Zakościelna 13</p>
+                            <p>26-500 Szydłowiec</p>
+                        </div>
 
-                    <div className="info-icon">
-                        <i className="icon-phone"></i>
-                    </div>
-                    <h3>Telefon</h3>
-                    <p>+48 574 922 900</p>
+                        {/* Karta z telefonem */}
+                        <div className="info-card">
+                            <div className="info-icon">
+                                <i className="pi pi-phone"></i>
+                            </div>
+                            <h3>Telefon</h3>
+                            <a href='tel:+48574922900'>+48 574 922 900</a>
+                        </div>
 
-                    <div className="info-icon">
-                        <i className="icon-email"></i>
+                        {/* Karta z adresem email */}
+                        <div className="info-card">
+                            <div className="info-icon">
+                                <i className="pi pi-envelope"></i>
+                            </div>
+                            <h3>Email</h3>
+                            <a href='mailto:swzygmuntszydlowiec@interia.pl'>swzygmuntszydlowiec@interia.pl</a>
+                        </div>
                     </div>
-                    <h3>Email</h3>
-                    <p>swzygmuntszydlowiec@interia.pl</p>
                 </div>
             </div>
         </div>
